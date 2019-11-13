fafbb79a134b815b95c2406f88147bd42b29fde0
CarolinaTawada <57716021+CarolinaTawada@users.noreply.github.com>
2019-11-13 10:52:58 -0300

    Add files via upload

---
+++ AULA05.php
@@ -0,0 +1,33 @@
+FILMES EM CARTAZ<BR>
+<?php
+  for($i =1; $i < 9; $i++){
+  echo 	"<img src='img/IMG_0".$i.".jpg' height='292' width='182'><br>";
+  echo  "IMG_0".$i.".jpg<br><br>";
+}
+?>
+
+<style> 
+<img src='img/IMG_01.jpg' height='292' width='182'>
+<br>IMG_01.jpg<br>
+
+<br><img src='img/IMG_02.jpg' height='292' width='182'>
+<br>IMG_02.jpg<br>
+
+<br><img src='img/IMG_03.jpg' height='292' width='182'>
+<br>IMG_03.jpg<br>
+
+<br><img src='img/IMG_04.jpg' height='292' width='182'>
+<br>IMG_04.jpg<br>
+
+<br><img src='img/IMG_05.jpg' height='292' width='182'>
+<br>IMG_05.jpg<br>
+
+<br><img src='img/IMG_06.jpg' height='292' width='182'>
+<br>IMG_06.jpg<br>
+
+<br><img src='img/IMG_07.jpg' height='292' width='182'>
+<br>IMG_07.jpg<br>
+
+<br><img src='img/IMG_08.jpg' height='292' width='182'>
+<br>IMG_08.jpg<br><br>
+
---
+++ Pag2.php
@@ -0,0 +1,39 @@
+<html>
+
+<style>
+.bgPage {background: #00FFFF;
+}
+float: right;
+</style>
+<?php
+for ($i=0;$i<10;$i=$i+1) {
+echo "<div class'bgPage'>";
+echo $.nome;
+echo "</div>;
+}
+?>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
